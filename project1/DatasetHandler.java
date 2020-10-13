56bfaf5ec476da9490601a7dd030fc9f3777c68f
Daniel Arredondo <edanielarredondo@gmail.com>
2020-10-13 10:32:33 -0500

    moved one print line out of loop in printReport()

--- project1/DatasetHandler.java
+++ project1/DatasetHandler.java
@@ -105,13 +105,13 @@ public class DatasetHandler {
 
 	public void printReport(final String dataID, int k) throws InvalidPathException, IOException {
 		
-		//implement method	
+		// implement method	
 		String report = " ";
 		List<AbstractRatingSummary> inList = getCollection(dataID).getRatingStat();
 		for (AbstractRatingSummary summary : inList) {
 			report += summary.toString() + "\n";
-			System.out.println(report);
 		}
+		System.out.println(report);
 		Path reportPath = this.defineReportPath(dataID);
 		Files.writeString(reportPath, report);
 
