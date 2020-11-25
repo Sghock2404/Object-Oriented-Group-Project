eb26d94d7e3ff125ce4e858ede43055b119b43bb
Himaja Akella <l_a338@txstate.edu>
2020-11-25 12:49:36 -0600

    add RatingStatsGUI; create Jframe, textfields

---
+++ project3/RatingStatsGUI.java
@@ -0,0 +1,55 @@
+package project3;
+
+
+//import java.util.Set;
+import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+public class RatingStatsGUI {
+
+    public static void main(String [] args) {
+		
+		String selection = "";
+        JFrame frame = new JFrame();
+	
+		try{
+			DatasetHandler dh = new DatasetHandler();
+			String setup = "Loading the datasets from:" + DataAnalysis.LINE_SEP
+            	+ "     data folder: " + dh.getFolderPath() + DataAnalysis.LINE_SEP
+                + "     datasets available: " + dh.getDataSets() + DataAnalysis.LINE_SEP;
+               
+
+        final int FIELD_WIDTH = 20;
+        final JTextField textField = new JTextField(FIELD_WIDTH);
+        textField.setText(setup);
+        dh.printDB();
+			
+        String welcomeMessage = "Choose one of the following functions:" + DataAnalysis.LINE_SEP
+            + "\t 1. Display computed statistics for specific dataID." + DataAnalysis.LINE_SEP
+            + "\t 2. Add new collection and compute statistics." + DataAnalysis.LINE_SEP
+            + "\t 0. Exit program." + DataAnalysis.LINE_SEP;
+        
+            final JTextField welcomeMsg = new JTextField(FIELD_WIDTH);
+            welcomeMsg.setText(welcomeMessage);
+
+            frame.setLayout(new FlowLayout());
+            frame.add(textField);
+            frame.add(welcomeMsg);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+            
+        }catch(IOException e){
+            System.out.println("Dataset path not found: " + e.getMessage());
+            System.out.println("Please check the file and try again, exiting.");
+        }   
+        
+       
+    }	
+
+    
+}
