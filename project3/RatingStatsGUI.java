ac19ff47a50fc01448d8f846e708e3ba4637b7e2
Himaja Akella <l_a338@txstate.edu>
2020-11-28 13:31:06 -0600

    update action event

--- project3/RatingStatsGUI.java
+++ project3/RatingStatsGUI.java
@@ -1,6 +1,6 @@
 package project3;
 
-//import java.util.Set;
+import java.util.Set;
 import java.io.IOException;
 import java.awt.*;
 import java.awt.event.*;
@@ -15,6 +15,7 @@ public class RatingStatsGUI extends JFrame {
     private JButton proceedButton;
     private JComboBox<String> dropDown;
     private int selectedIndex;
+    static JTextField textField;
 
     // This sets up the GUI Frame (constructor)
     RatingStatsGUI() {
@@ -54,16 +55,47 @@ public class RatingStatsGUI extends JFrame {
         });
 
         // Add event listener for proceed button
-        proceedButton.addActionListener(new ActionListener() {
 
+        try {
+        DatasetHandler dh = new DatasetHandler();
+        Set<Dataset> datasets = dh.getDataSets();
+
+        proceedButton.addActionListener(new ActionListener() {
+        
             @Override
             public void actionPerformed(ActionEvent event) {
-                String selection = (String) dropDown.getSelectedItem();
-                JOptionPane.showMessageDialog(RatingStatsGUI.this, "You selected option" + selectedIndex);
-            }
-        });
+                if (selectedIndex == 0) {
+                        if (datasets.size()<1){
+                        String selection = (String) dropDown.getSelectedItem();
+                        JOptionPane.showMessageDialog(RatingStatsGUI.this, "There is no data to select from, select another option");
+                    }
+                    else {
+                        //dh.printDB();
+                        
+                        String selection = (String) dropDown.getSelectedItem();
+                        JOptionPane.showMessageDialog(RatingStatsGUI.this, "Please enter dataID from the list");
+                    }
+                }
 
-    }
+                if (selectedIndex == 1) {
+                    String selection = (String) dropDown.getSelectedItem();
+                    JOptionPane.showMessageDialog(RatingStatsGUI.this, "Please enter new unique dataID"); 
+                    
+
+                }
+
+                if (selectedIndex == 2) {
+                    String selection = (String) dropDown.getSelectedItem();
+                    JOptionPane.showMessageDialog(RatingStatsGUI.this, "Goodbye!"); 
+                }
+        }
+    });
+
+    }catch(IOException e){
+		System.out.println("Dataset path not found: " + e.getMessage());
+		System.out.println("Please check the file and try again, exiting.");
+	}
+}
 
     public int getSelectedIndex() {
         return selectedIndex;
@@ -76,9 +108,12 @@ public class RatingStatsGUI extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        
+        
 
         int selection = frame.getSelectedIndex();
             
     }
 
+    
 }
